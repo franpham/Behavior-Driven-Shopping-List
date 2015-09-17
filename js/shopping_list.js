85448bdd27d1c438c43632516ae7a780098228c0
franpham <franpham@gmail.com>
2015-09-16 22:55:24 -1000

    initial tests & code for ShoppingList

---
+++ js/shopping_list.js
@@ -0,0 +1,39 @@
+
+function ShoppingList() {
+  this.items = [];
+}
+
+ShoppingList.prototype.addItem = function(item) {
+  if (item instanceof ShoppingListItem) {
+    this.items.push(item);
+    return true;
+  } else {
+    throw new Error('wrong item type');
+  }
+};
+
+ShoppingList.prototype.removeItem = function(item) {
+  if (item === null) {
+    if (this.items.length > 0)
+      this.items.pop();
+    else
+      return false;
+  } else if (item instanceof ShoppingListItem) {
+    var index = list.indexOf(item);
+    if (index >= 0)
+      list.splice(index, 1);
+    else
+      return false;
+  } else {
+    throw new Error('wrong item type');
+  }
+  return true;
+};
+
+ShoppingList.prototype.render = function() {
+  var str = '<ul>';
+  for (var i = 0; i < this.items.length; i++) {
+    str += this.items[i].render();
+  }
+  return str + '</ul>';
+};
--- js/shopping_list_test.js
+++ js/shopping_list_test.js
@@ -1,12 +1,65 @@
 // mocha.js, chai.js, shopping-list-test.js are loaded by script tags in index.html
 var expect = chai.expect;
 
+describe('ShoppingList', function() {
+  var list, item;
+  beforeEach(function() {
+    list = new ShoppingList();
+    item = new ShoppingListItem('item', 'test');
+  });
+  it("ShoppingList should be a class", function() {
+    expect(ShoppingList).to.be.a('function');
+    expect(ShoppingList.prototype).to.be.an('object');
+  });
+  it("ShoppingList has an items property", function() {
+    expect(list).has.property('items');
+  });
+  it("ShoppingList has a constructor that creates an empty Array", function() {
+    expect(list.items).to.be.instanceof(Array);
+    expect(list.items).to.be.empty;
+  });
+  it("ShoppingList has an addItem method that takes an argument", function() {
+    expect(ShoppingList).respondTo('addItem');
+    expect(ShoppingList.prototype.addItem).to.have.length(1);
+  });
+  it("ShoppingList's addItem puts a ShoppingListItem in items Array", function() {
+    expect(list.addItem(item)).to.be.true;
+    expect(list.items).to.have.length(1);
+  });
+  it("ShoppingList's addItem throws an error if the item is not a ShoppingListItem", function() {
+    expect(list.addItem(new Object())).to.throw(Error);
+  });
+  it("ShoppingList has a removeItem method that takes an argument", function() {
+    expect(ShoppingList).respondTo('removeItem');
+    expect(ShoppingList.prototype.removeItem).to.have.length(1);
+  });
+  it("ShoppingList's removeItem removes a ShoppingListItem from items Array if it's present", function() {
+    list.addItem(item);
+    expect(list.removeItem(item)).to.be.true;
+    expect(list.items).to.have.length(0);
+  });
+  it("ShoppingList's removeItem removes the last item if no argument was given", function() {
+    list.addItem(item);
+    expect(list.removeItem()).to.be.true;
+    expect(list.items).to.have.length(0);
+  });
+  it("ShoppingList's removeItem throws an error if item is not a ShoppingListItem", function() {
+    expect(list.removeItem(new Object())).to.throw(Error);
+  });
+  it("ShoppingList has a render method that returns a HTML list of each of the items", function() {
+    list.addItem(item);
+    var str = list.render();
+    expect(str).to.have.string('<ul><li');
+    expect(str).to.have.string('/li></ul>');
+  });
+});
+
 describe('ShoppingListItem', function() {
   var item;
   beforeEach(function() {
     item = new ShoppingListItem('item', 'test');
   });
-  it("ShoppingListItem should be a function", function() {
+  it("ShoppingListItem should be a class", function() {
     expect(ShoppingListItem).to.be.a('function');
     expect(ShoppingListItem.prototype).to.be.an('object');
   });
