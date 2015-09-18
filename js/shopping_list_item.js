87e875601ef470066614bab7c7a3bb85581f7821
franpham <franpham@gmail.com>
2015-09-17 21:13:11 -1000

    modified render to use html()

--- js/shopping_list_item.js
+++ js/shopping_list_item.js
@@ -15,10 +15,6 @@ ShoppingListItem.prototype.uncheck = function uncheck() {
 
 ShoppingListItem.prototype.render = function render() {
   var el = $('<div />').append($('<li />').addClass('completed_' + this.is_done).append($('<span />').text(this.name + ': ')).append($('<span />').text(this.description)));
-  var str = '';                           // el refers to the FIRST element created, <div>;
-  el.children().each(function() {         // html() returns only the html of the FIRST element, so must use each to iterate thru children;
-    str += $(this).prop('outerHTML');     // must use prop('outerHTML') to get the current element also;
-  });
-  // console.log(str);
-  return str;
+  // create outer div since html() returns innerHTML; use prop('outerHTML') to get current element also;
+  return el.html();            // el refers to the FIRST element created, <div>
 };
